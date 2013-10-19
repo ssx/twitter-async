6d564bcce31f934a7f57dc0da2e79879c6f0ba73
Scott Wilcox <scott@dor.ky>
2013-10-19 12:05:22 +0100

    Begin removing basic auth relating aspects of the library Updated simpleTest.php with a couple of new examples

--- simpleTest.php
+++ simpleTest.php
@@ -5,20 +5,34 @@ require "vendor/autoload.php";
 // Test credentials file
 require "TestCredentials.php";
 
-$twitterObj = new SSX\EpiTwitter($consumer_key, $consumer_secret, $token, $secret);
-$twitterObjUnAuth = new SSX\EpiTwitter($consumer_key, $consumer_secret);
+$twitterObj = new \SSX\EpiTwitter($consumer_key, $consumer_secret, $token, $secret);
+$twitterObjUnAuth = new \SSX\EpiTwitter($consumer_key, $consumer_secret);
 ?>
 
-<h2>Generate the authorization link</h2>
+<h2>Generate an authorisation link</h2>
 <?php echo $twitterObjUnAuth->getAuthenticateUrl(); ?>
 
 <hr>
 
-<h2>Verify credentials</h2>
+<?php $creds = $twitterObj->get('/account/verify_credentials.json'); ?>
+<pre><?php print_r($creds->response); ?></pre>
+
+<hr>
+
+<h2>Get followers (first cursor)</h2>
 <?php
-  $creds = $twitterObj->get('/account/verify_credentials.json');
+  $followers = $twitterObj->get("/followers/ids.json", array(
+  	"screen_name" => "ssx"
+  ));
 ?>
-<pre>
-<?php print_r($creds->response); ?>
-</pre>
+<pre><?php print_r($followers->response); ?></pre>
+
+<hr>
+
+<h2>Post a status update</h2>
+<?php
+$twitterObj->post('/statuses/update.json', array(
+  	"status" => "Hello from twitter-aysnc!"
+));
+?>
 
--- src/EpiTwitter.php
+++ src/EpiTwitter.php
@@ -16,15 +16,15 @@ class EpiTwitter extends EpiOAuth
 {
   const EPITWITTER_SIGNATURE_METHOD = 'HMAC-SHA1';
   const EPITWITTER_AUTH_OAUTH = 'oauth';
-  const EPITWITTER_AUTH_BASIC = 'basic';
   protected $requestTokenUrl= 'https://api.twitter.com/oauth/request_token';
   protected $accessTokenUrl = 'https://api.twitter.com/oauth/access_token';
   protected $authorizeUrl   = 'https://api.twitter.com/oauth/authorize';
   protected $authenticateUrl= 'https://api.twitter.com/oauth/authenticate';
-  protected $apiUrl         = 'http://api.twitter.com';
-  protected $userAgent      = 'EpiTwitter (http://github.com/jmathai/twitter-async/tree/)';
+  protected $apiUrl         = 'https://api.twitter.com';
+  protected $userAgent      = 'EpiTwitter (http://github.com/ssx/twitter-async/tree/)';
   protected $apiVersion     = '1.1';
   protected $isAsynchronous = false;
+
   /**
    * The Twitter API version 1.0 search URL.
    * @var string
@@ -45,22 +45,6 @@ class EpiTwitter extends EpiOAuth
   public function post($endpoint, $params = null)
   {
     return $this->request('POST', $endpoint, $params);
-  }
-
-  /* Basic auth methods */
-  public function delete_basic($endpoint, $params = null, $username = null, $password = null)
-  {
-    return $this->request_basic('DELETE', $endpoint, $params, $username, $password);
-  }
-
-  public function get_basic($endpoint, $params = null, $username = null, $password = null)
-  {
-    return $this->request_basic('GET', $endpoint, $params, $username, $password);
-  }
-
-  public function post_basic($endpoint, $params = null, $username = null, $password = null)
-  {
-    return $this->request_basic('POST', $endpoint, $params, $username, $password);
   }
 
   public function useApiUrl($url = '')
@@ -94,65 +78,21 @@ class EpiTwitter extends EpiOAuth
     $endpoint = str_replace('//','/',$endpoint);
     $args = !empty($params) ? array_shift($params) : null;
 
-    // calls which do not have a consumerKey are assumed to not require authentication
-    if($this->consumerKey === null)
-    {
-      $username = null;
-      $password = null;
-
-      if(!empty($params))
-      {
-        $username = array_shift($params);
-        $password = !empty($params) ? array_shift($params) : null;
-      }
-
-      return $this->request_basic($method, $endpoint, $args, $username, $password);
-    }
-
     return $this->request($method, $endpoint, $args);
   }
 
   private function getApiUrl($endpoint)
   {
-    if ($this->apiVersion === '1' && preg_match('@^/search[./]?(?=(json|daily|current|weekly))@', $endpoint))
-    {
-      return $this->searchUrl.$endpoint;
-    }
-
     return $this->apiUrl.'/'.$this->apiVersion.$endpoint;
   }
 
   private function request($method, $endpoint, $params = null)
   {
     $url = $this->getUrl($this->getApiUrl($endpoint));
-    $resp= new EpiTwitterJson(call_user_func(array($this, 'httpRequest'), $method, $url, $params, $this->isMultipart($params)), $this->debug);
-    if(!$this->isAsynchronous)
-      $resp->response;
 
-    return $resp;
-  }
+	echo "URL: '$url' \n";
 
-  private function request_basic($method, $endpoint, $params = null, $username = null, $password = null)
-  {
-    $url = $this->getApiUrl($endpoint);
-    if($method === 'GET')
-      $url .= is_null($params) ? '' : '?'.http_build_query($params, '', '&');
-    $ch  = curl_init($url);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Expect:'));
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_TIMEOUT, $this->requestTimeout);
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
-    if($method === 'POST' && $params !== null)
-    {
-      if($this->isMultipart($params))
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
-      else
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $this->buildHttpQueryRaw($params));
-    }
-    if(!empty($username) && !empty($password))
-      curl_setopt($ch, CURLOPT_USERPWD, "{$username}:{$password}");
-
-    $resp = new \SSX\EpiTwitterJson(EpiCurl::getInstance()->addCurl($ch), $this->debug);
+    $resp= new \SSX\EpiTwitterJson(call_user_func(array($this, 'httpRequest'), $method, $url, $params, $this->isMultipart($params)), $this->debug);
     if(!$this->isAsynchronous)
       $resp->response;
 
